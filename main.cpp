0ebca29ee7e60f43bdda9364ac75156fb5598f30
Skerdi <81383971+skerdivelo@users.noreply.github.com>
2023-10-22 12:06:10 +0000

    trapec

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    float a, b, c, d;
+    cout <<"lice a na trapec"<<endl; 
+    cin >> a;   
+    cout <<"lice b na trapec"<<endl; 
+    cin >> b;
+    cout <<"lice c na trapec"<<endl; 
+    cin >> c;
+    cout <<"lice d na trapec"<<endl;
+    cin >> d;   
+    cout << "P na trapec e: " << a+b+c << endl;
+    cout << "liceto na trapec: "<<(a+b)*c/2 <<endl;
+    return 0;
+}
