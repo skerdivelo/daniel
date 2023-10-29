31196dd92e00371ec748c977a33a97822afaf176
Skerdi <81383971+skerdivelo@users.noreply.github.com>
2023-10-29 13:33:38 +0000

    everything works fine ```

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 
-int main()
-{
-    float a, b, c, d;
-    cout <<"lice a na trapec"<<endl; 
-    cin >> a;   
-    cout <<"lice b na trapec"<<endl; 
-    cin >> b;
-    cout <<"lice c na trapec"<<endl; 
-    cin >> c;
-    cout <<"lice d na trapec"<<endl;
-    cin >> d;   
-    cout << "P na trapec e: " << a+b+c << endl;
-    cout << "liceto na trapec: "<<(a+b)*c/2 <<endl;
-    return 0;
+int main(){
+    double const basePrice = 800.00;
+    int speedOverLimit = 0;
+    double fine = 0.0;
+    cout << "Enter the speed limit: ";
+    cin >> speedOverLimit;
+    
+    if(speedOverLimit <= 20){ //range 0-20
+        fine = basePrice * 0.15;
+    }else if(speedOverLimit > 20 && speedOverLimit <= 40){ //range 21-40
+        fine = basePrice * 0.25;
+    }else if(speedOverLimit > 40 && speedOverLimit <= 50){ //range 41-50
+        fine = basePrice * 0.4;
+    }else if(speedOverLimit > 50 && speedOverLimit <= 60){ //range 51-60
+        fine = basePrice * 0.7;
+    }else{
+        cout << "You are going to jail!" << endl;
+    }
+    cout << "Your fine is: €" << fine << endl;
 }
